5aacee6e951ae3dda99d880c53fad4ef7bdce377
Silvestre Huens <s.huens@gmail.com>
2012-02-15 09:40:35 -0300

    initial product structure

---
+++ s17/media/views/events.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from five import grok
+from Products.ATContentTypes.interfaces import IATFile
+from Products.Archetypes.interfaces import IObjectInitializedEvent
+
+@grok.subscribe(IATFile, IObjectInitializedEvent)
+def set_default_layout(obj, event):
+    ct = obj.getContentType()
+    if ct:
+        if 'video' in ct:
+            obj.setLayout('video_file')
+        if 'audio' in ct:
+            obj.setLayout('audio_file')
