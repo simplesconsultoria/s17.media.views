0007d2196592cce919a9b51e1bfc461c28766f9b
hvelarde <hector.velarde@gmail.com>
2012-02-17 18:21:58 -0200

    fixed subscriber

--- s17/media/views/events.py
+++ s17/media/views/events.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
+
 from five import grok
+
 from Products.ATContentTypes.interfaces import IATFile
 from Products.Archetypes.interfaces import IObjectInitializedEvent
 from Products.Archetypes.interfaces.event import IObjectEditedEvent
+
 
 @grok.subscribe(IATFile, IObjectInitializedEvent)
 def set_default_layout(obj, event):
@@ -13,17 +16,15 @@ def set_default_layout(obj, event):
         elif 'audio' in ct and obj.getFilename().split('.')[-1].lower() == 'mp3':
             obj.setLayout('audio_file')
 
+
 @grok.subscribe(IATFile, IObjectEditedEvent)
 def set_default_layout(obj, event):
     ct = obj.getContentType()
     layout = obj.getLayout()
-    ext = obj.getFilename().split('.')[-1].lower()
     if ct:
-        if 'video' in ct and layout != 'video_file':
+        if 'video' in ct:
             obj.setLayout('video_file')
-        elif 'audio' in ct and ext == 'mp3' and layout != 'audio_file':
+        elif 'audio' in ct:
             obj.setLayout('audio_file')
         elif layout != 'file_view':
             obj.setLayout('file_view')
-        
-    
