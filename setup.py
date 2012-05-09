9027336a081813312e4b9a469075d145684346cc
hvelarde <hector.velarde@gmail.com>
2012-05-09 15:03:49 -0300

    package promoted to Beta

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(name='s17.media.views',
       description="Media views for multimedia files",
       long_description=long_description,
       classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 4 - Beta",
         "Environment :: Web Environment",
         "Framework :: Plone",
         "Framework :: Plone :: 4.1",
